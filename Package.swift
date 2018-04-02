1da4b2f66ef1c2a08a49d54f9a3b1799d5b284c1
Roopesh Chander <roop@roopc.net>
2018-04-02 14:41:17 +0530

    Blake2s: Add Blake2s 3rdparty code as a separate target
    Source: https://github.com/BLAKE2/BLAKE2

--- Package.swift
+++ Package.swift
@@ -20,6 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NEWireGuard",
+            dependencies: ["Blake2s"]),
+        .target(
+            name: "Blake2s",
             dependencies: []),
         .testTarget(
             name: "NEWireGuardTests",
---
+++ Sources/Blake2s/README.txt
@@ -0,0 +1,1 @@
+Blake2 C implementation from the ref/ folder in https://github.com/BLAKE2/BLAKE2
