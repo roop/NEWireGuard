5665097fb0d1ffd8b435967f5135e6bb4847ec2a
Roopesh Chander <roop@roopc.net>
2018-04-06 23:07:50 +0530

    Fix NEWireGuard test

--- Tests/NEWireGuardTests/NEWireGuardTests.swift
+++ Tests/NEWireGuardTests/NEWireGuardTests.swift
@@ -3,10 +3,7 @@ import XCTest
 
 class NEWireGuardTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(NEWireGuard().text, "Hello, World!")
+        XCTAssertEqual(Noise.noiseProtocolName, "Noise_IKpsk2_25519_ChaChaPoly_BLAKE2s")
     }
 
 
